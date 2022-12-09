fc21e1961c1f17b15b0e511856397cacfabcba95
kisrr <joshyfoshydick@gmail.com>
2022-12-09 16:03:25 +0800

    add / remove commands

--- src/main/java/me/kisr/commands/CommandManager.java
+++ src/main/java/me/kisr/commands/CommandManager.java
@@ -2,8 +2,10 @@ package me.kisr.commands;
 
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,22 @@ public class CommandManager extends ListenerAdapter {
          */
 
         commands.add(Commands.slash("sendapplication", "Send the create application embed."));
+
+        /*
+        add command
+         */
+
+        OptionData addUser = new OptionData(OptionType.USER, "user", "Person to add to the ticket.", true);
+
+        commands.add(Commands.slash("add", "Add someone to a ticket.").addOptions(addUser));
+
+        /*
+        remove command
+         */
+
+        OptionData removeUser = new OptionData(OptionType.USER, "user", "Person to remove to the ticket.", true);
+
+        commands.add(Commands.slash("remove", "Remove someone to a ticket.").addOptions(removeUser));
 
         /*
         update guild commands
--- src/main/java/me/kisr/commands/Commands.java
+++ src/main/java/me/kisr/commands/Commands.java
@@ -1,5 +1,8 @@
 package me.kisr.commands;
 
+import me.kisr.Main;
+import me.kisr.utils.FileUtils;
+import me.kisr.utils.RoleUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -7,7 +10,9 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.awt.*;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 public class Commands extends ListenerAdapter {
@@ -33,6 +38,62 @@ public class Commands extends ListenerAdapter {
             } else {
                 event.reply("No permission!").setEphemeral(true).queue();
             }
+        }
+
+        /*
+        add command
+         */
+
+        else if (command.equals("add")) {
+            if (event.getMember().hasPermission(Permission.ADMINISTRATOR) || RoleUtils.hasTester(event.getMember())) {
+                if (event.getMember().equals(event.getOption("user").getAsMember())) {
+                    event.reply("You can't add yourself to the ticket!").queue();
+                } else {
+                    if (event.getOption("user").getAsMember().getUser().isBot()) {
+                        event.reply("You can't add a bot to a ticket!").setEphemeral(true).queue();
+                    } else {
+                        if (event.getGuild().getCategoryById(Main.config.get("APPLICATION_CATEGORY")).getChannels().contains(event.getChannel())) {
+                            event.getChannel().asTextChannel().getManager().putPermissionOverride(event.getOption("user").getAsMember(), EnumSet.of(Permission.VIEW_CHANNEL), null).queue();
+                            event.reply("Added " + event.getOption("user").getAsMember().getAsMention() + " to the ticket!").queue();
+                        } else {
+                            event.reply("You have to be in a ticket to use this command!").setEphemeral(true).queue();
+                        }
+                    }
+                }
+            } else {
+                event.reply("No permission!").setEphemeral(true).queue();
+            }
+        }
+
+        /*
+        remove command
+         */
+
+        else if (command.equals("remove")) {
+            if (event.getMember().hasPermission(Permission.ADMINISTRATOR) || RoleUtils.hasTester(event.getMember())) {
+                String[] datasplit = FileUtils.readFile(new File("files/tickets/" + event.getChannel().getId())).split("36280b7179c69735773b65d0c595d3f1114400367759a2a21a22870e14a4ae47");
+                if (event.getGuild().getMemberById(datasplit[0]).equals(event.getOption("user").getAsMember())) {
+                    event.reply("You can't remove the owner of the ticket!").setEphemeral(true).queue();
+                } else {
+                    if (event.getMember().equals(event.getOption("user"))) {
+                        event.reply("You can't remove yourself from the ticket!");
+                    } else {
+                        if (event.getOption("user").getAsMember().getUser().isBot()) {
+                            event.reply("You can't remove a bot from a ticket!").setEphemeral(true).queue();
+                        } else {
+                            if (event.getGuild().getCategoryById(Main.config.get("APPLICATION_CATEGORY")).getChannels().contains(event.getChannel())) {
+                                event.getChannel().asTextChannel().getManager().putPermissionOverride(event.getOption("user").getAsMember(), null, EnumSet.of(Permission.VIEW_CHANNEL)).queue();
+                                event.reply("Removed " + event.getOption("user").getAsMember().getAsMention() + " to the ticket!").queue();
+                            } else {
+                                event.reply("You have to be in a ticket to use this command!").setEphemeral(true).queue();
+                            }
+                        }
+                    }
+                }
+            }
+
+        } else {
+            event.reply("No permission!").setEphemeral(true).queue();
         }
     }
 }
